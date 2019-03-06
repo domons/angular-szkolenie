9026056ff18d2149c8956aa0048aec739bcd9e2a
Dominik Barylski <barylski.dominik@gmail.com>
2019-03-06 15:32:56 +0100

    list

--- src/app/components/move-list/move-list.component.ts
+++ src/app/components/move-list/move-list.component.ts
@@ -7,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MoveListComponent implements OnInit {
 
+  movieList = [
+    {
+      title: 'Film #1',
+      description: 'Lorem ipsum 1',
+      image: 'https://avatars0.githubusercontent.com/u/10672914?s=460&v=4',
+    },
+    {
+      title: 'Film #2',
+      description: 'Lorem ipsum 2',
+      image: 'https://avatars0.githubusercontent.com/u/10672914?s=460&v=4',
+    },
+    {
+      title: 'Film #3',
+      description: 'Lorem ipsum 3',
+      image: 'https://avatars0.githubusercontent.com/u/10672914?s=460&v=4',
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {

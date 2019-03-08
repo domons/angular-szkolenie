f7cd5df3da8e8b6e646eceb2b64e908c95deab83
Dominik Barylski <barylski.dominik@gmail.com>
2019-03-08 08:55:34 +0100

    output decorator

--- src/app/core/pages/page-notfound/page-notfound.component.ts
+++ src/app/core/pages/page-notfound/page-notfound.component.ts
@@ -5,11 +5,34 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './page-notfound.component.html',
   styleUrls: ['./page-notfound.component.css']
 })
-export class PageNotfoundComponent implements OnInit {
+export class PageNotfoundComponent {
 
-  constructor() { }
+  playerStatus = 'Loading';
 
-  ngOnInit() {
+  movie = {
+    "id": "2zR1GOLhlU4",
+    "title": "Hellboy",
+    "description": "",
+    "thumb_url": "http://i3.ytimg.com/vi/2zR1GOLhlU4/hqdefault.jpg",
+    "movie_url": "https://www.youtube.com/embed/2zR1GOLhlU4",
+
+    "year": 2019,
+    "duration": 120,
+    "rate": 6.66
+  };
+
+  handleStatus(e) {
+    console.log('test', e);
+    switch(e.status) {
+      case 'playing':
+        this.playerStatus = 'Player is playing';
+        break;
+      case 'paused':
+        this.playerStatus = 'Player is paused';
+        break;
+      default:
+        this.playerStatus = 'Player is idle';
+    }
   }
 
 }

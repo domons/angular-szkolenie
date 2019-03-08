bd73c259f98ca67ad51321fa92fc284eb62e2de7
Dominik Barylski <barylski.dominik@gmail.com>
2019-03-08 10:49:38 +0100

    test

--- src/app/core/components/app/app.component.spec.ts
+++ src/app/core/components/app/app.component.spec.ts
@@ -32,10 +32,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('movies');
   });
 
-  xit('should render title in a h1 tag', () => {
+  it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to movies!');
+    expect(compiled.querySelector('h1').textContent).toContain('Movies');
   });
 });
